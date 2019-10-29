af3b7c724ce55da919aef31f018776c7be2c0a2f
manishkumr <rishimanish123@gmail.com>
2019-10-29 15:17:08 +1100

    using custom hooks and async await to fetch data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useFetch from "./useFetch"
 
-function App() {
+function Button () {
+  const [buttonText, setButtonText] = useState("click me!");
+  const users = useFetch('https://jsonplaceholder.typicode.com/users/');
+  const [data, setData] = useState([]);
+  function handleClick() {
+      getData();
+
+  }
+  async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    setData(data)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <button onClick={handleClick}>{"Fetch Data"}</button>
+        <ul>
+          {data.map(el => (
+              <li key={el.id}>{el.title}</li>
+          ))}
+        </ul>
+        <ul>
+          {users.map(el => (
+              <li key={el.id}>{el.name}</li>
+          ))}
+        </ul>
+      </div>
   );
 }
-
-export default App;
+export default Button;
